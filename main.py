6d6c42f3c5ab5eb9f03b31013a0ff72cef9232d7
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-10-17 14:32:34 +0200

    implement lookup of last build on startup implement config file implement http basic authentication

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from ws4py.client.threadedclient import WebSocketClient
 import RPi.GPIO as GPIO
-import threading, time, json
+import threading, time, json, urllib2, base64, ConfigParser
 
 class JenkinsClient(WebSocketClient):
+	def __init__(self, host, httpPort, wsPort, project, user, token):
+		self.host = host
+		self.httpPort = httpPort
+		self.wsPort = wsPort
+		self.project = project
+		self.user = user
+		self.token = token
+		super(JenkinsClient, self).__init__('ws://' + self.host + ':' + str(self.wsPort) + '/jenkins')
+
 	def opened(self):
 		print "Connection opened"
 
@@ -11,18 +20,35 @@ class JenkinsClient(WebSocketClient):
 		print "Closed down", code, reason
 
 	def received_message(self, m):
+		print "=> %d %s" % (len(m), str(m))
+		message = json.loads(str(m));
+		self.update(message)
+
+	def start(self):
+		self.connect()
+		self.readCurrentState()
+
+	def update(self, message):
+		if project in message and message['project'] != project: return
+		# turn all LEDs off
 		GPIO.output(11, GPIO.LOW)
 		GPIO.output(12, GPIO.LOW)
 		GPIO.output(13, GPIO.LOW)
-		print "=> %d %s" % (len(m), str(m))
-		message = json.loads(str(m));
-		if message['project'] == project:
-			if message['result'] == 'SUCCESS':
-				GPIO.output(13, GPIO.HIGH)
-			elif message['result'] == 'UNSTABLE':
-				GPIO.output(12, GPIO.HIGH)
-			elif message['result'] == 'FAILURE':
-				GPIO.output(11, GPIO.HIGH)
+		# turn on only the one that reflects our status
+		if message['result'] == 'SUCCESS':
+			GPIO.output(13, GPIO.HIGH)
+		elif message['result'] == 'UNSTABLE':
+			GPIO.output(12, GPIO.HIGH)
+		elif message['result'] == 'FAILURE':
+			GPIO.output(11, GPIO.HIGH)
+
+	def readCurrentState(self):
+		auth = base64.encodestring('%s:%s' % (self.user, self.token)).replace('\n', '')
+		req = urllib2.Request('http://' + self.host + ':' + str(self.httpPort) + '/job/' + self.project + '/lastBuild/api/json')
+		req.add_header('Authorization', 'Basic: %s' % auth)
+		res = urllib2.urlopen(req)
+		message = json.loads(res.read())
+		self.update(message)
 
 if __name__ == '__main__':
 	GPIO.setmode(GPIO.BOARD)
@@ -33,11 +59,19 @@ if __name__ == '__main__':
 	GPIO.setup(13, GPIO.OUT)
 	GPIO.output(13, GPIO.LOW)
 
-	project = 'Crawler'
+	project = 'Autorensystem'
+
+	config = ConfigParser.ConfigParser()
+	config.read('config.ini')
 
 	try:
-		ws = JenkinsClient('ws://dev-build.pocci.cxo.name:8000/jenkins', protocols=['http-only', 'chat'])
-		ws.connect()
+		ws = JenkinsClient(config.get('jenkins', 'host'),
+				   config.get('jenkins', 'httpPort'),
+				   config.get('jenkins', 'wsPort'),
+				   config.get('project', 'name'),
+				   config.get('jenkins', 'user'),
+				   config.get('jenkins', 'token'));
+		ws.start()
 		while threading.active_count() > 0 :
 			time.sleep(1)
 
