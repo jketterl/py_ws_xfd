a8f70678aaef3ec65225fca3f4494cefd06786cd
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-12-13 20:53:41 +0000

    add a script that monitors a gpio and configures the network accordingly

---
+++ networkmanager.py
@@ -0,0 +1,35 @@
+from RPi import GPIO
+import time
+import subprocess
+
+def enableAccessPoint():
+  print "enabling access point..."
+  subprocess.call(['/sbin/ifdown', 'wlan0'])
+  subprocess.call(['/sbin/ifconfig', 'wlan0', '192.168.99.1'])
+  subprocess.call(['/usr/sbin/hostapd', '-B', '/etc/hostapd/hostapd.conf', '-P', '/home/pi/hostapd.pid'])
+  subprocess.call(['/usr/sbin/service', 'isc-dhcp-server', 'start'])
+
+def disableAccessPoint():
+  print "disabling access point..."
+  subprocess.call(['/usr/sbin/service', 'isc-dhcp-server', 'stop'])
+  f = open('/home/pi/hostapd.pid', 'r')
+  pid = f.readline().rstrip('\n')
+  f.close()
+  subprocess.call(['/bin/kill', pid])
+  subprocess.call(['/sbin/ifconfig', 'wlan0', 'down'])
+  subprocess.call(['/sbin/ifup', 'wlan0'])
+
+def switch(channel):
+  if GPIO.input(channel):
+    enableAccessPoint()
+  else:
+    disableAccessPoint();
+
+if __name__ == '__main__':
+  GPIO.setmode(GPIO.BCM)
+
+  GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+  if (GPIO.input(4)): enableAccessPoint()
+
+  GPIO.add_event_detect(4, GPIO.BOTH, callback = switch)
+  while True: time.sleep(1)
