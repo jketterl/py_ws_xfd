f3e4f629ef48474fabae6649530c4efd46d14289
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-01-23 00:05:33 +0000

    add signal handling

--- main.py
+++ main.py
@@ -3,13 +3,26 @@
 import threading
 import time
 import logging
-import control
+import signal
 
+import control
 import jenkins
 
 logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
 
+
+class SignalException(Exception):
+    pass
+
+
+def handle_signal(sig, frame):
+    raise SignalException("Caught signal {0}".format(sig))
+
+
 if __name__ == '__main__':
+
+    for sig in [signal.SIGINT, signal.SIGTERM]:
+        signal.signal(sig, handle_signal)
 
     # initialize component lists
     serverList = jenkins.ServerList("servers.json")
@@ -20,7 +33,7 @@ if __name__ == '__main__':
         while threading.active_count() > 1:
             time.sleep(20)
 
-    except (KeyboardInterrupt, SystemExit):
+    except SignalException:
         logging.info("shutting down")
         control.ControlServer.getInstance().shutdown()
         for output in outputList.all():
